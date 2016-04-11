f3a2e18ddd32a028bee87d857bf2de7d12cbf0c2
vplrt <djangositedev@gmail.com>
2016-04-11 17:57:26 +0300

    Fix exceptions in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,29 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
-  rescue_from CanCan::AccessDenied do
-    respond_to do |format|
-      format.html { not_autorized }
-      format.js   { render nothing: true, status: :forbidden }
-    end
-  end
+  rescue_from CanCan::AccessDenied, with: :formatted_not_autorized
+
+  rescue_from ActiveRecord::RecordNotFound,
+              ActionController::RoutingError,
+              ActiveRecord::RecordInvalid,
+              with: :redirect_with_error
 
   private
 
   def layout_by_resource
     devise_controller? ? 'devise_layout' : 'application'
+  end
+
+  def redirect_with_error
+    flash[:error] = 'Something goes wrong'
+    redirect_to root_path
+  end
+
+  def formatted_not_autorized
+    respond_to do |format|
+      format.html { not_autorized }
+      format.js   { render nothing: true, status: :forbidden }
+    end
   end
 
   def not_autorized
